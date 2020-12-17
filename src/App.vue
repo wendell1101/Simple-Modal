dded2aa2316a9afc7b0b65431c5d10470f6609c6
Wendell Suazo <wendellchansuazo11@gmail.com>
2020-12-17 16:40:28 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{ title }}</h1>
+    <div v-if="this.showModal">
+       <Modal :header="this.header" :text="this.text" theme="sale" @close="this.toggleModal"/>
+    </div>
+    <br>
+    <button @click="toggleModal">
+        Open modal
+    </button>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal,
+  },
+  data () {
+    return{
+      title : 'Standard Modal',
+      header: "Sign up for the holiday promo",
+      text: "Grab yours now",
+      showModal : false,
+    }
+  },
+  methods : {
+    handleClick(){
+      this.$refs.name.classList.toggle('active');
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
   }
 }
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <style>
 #app {
@@ -22,5 +62,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        
+        <div class="modal">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['header','text', 'theme'],
+    methods : {
+        closeModal (){
+            this.$emit('close')
+        }
+    }
+}
+</script>
+
+<style>
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top:0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1{
+        color:rgb(51, 79, 236);
+    }
+    .modal p{
+        font-style: normal;
+    }
+    .sale{
+        background:crimson;
+        color: #fff;
+    }
+    .sale h1{
+        color: #fff;
+    }
+</style>
+
+
+
